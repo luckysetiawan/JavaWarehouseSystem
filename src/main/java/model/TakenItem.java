f8ea9a366f0006a886d8e0b60efbca8e4bf959f1
Julian <julianely23@gmail.com>
2020-11-12 19:59:25 +0700

    update controller

--- src/main/java/model/TakenItem.java
+++ src/main/java/model/TakenItem.java
@@ -14,14 +14,13 @@ import java.util.ArrayList;
 public class TakenItem {
     private int taken_id;
     private ArrayList <Item> listItem;
-    private int uid, item_quantity;
+    private int uid;
     private String date;
 
-    public TakenItem(int taken_id, ArrayList<Item> listItem, int uid, int item_quantity, String date) {
+    public TakenItem(int taken_id, ArrayList<Item> listItem, int uid, String date) {
         this.taken_id = taken_id;
         this.listItem = listItem;
         this.uid = uid;
-        this.item_quantity = item_quantity;
         this.date = date;
     }
 
@@ -47,14 +46,6 @@ public class TakenItem {
 
     public void setUid(int uid) {
         this.uid = uid;
-    }
-
-    public int getItem_quantity() {
-        return item_quantity;
-    }
-
-    public void setItem_quantity(int item_quantity) {
-        this.item_quantity = item_quantity;
     }
 
     public String getDate() {
