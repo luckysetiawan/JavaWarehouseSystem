907e8e832fa3fbee1b03dce012f3942896ccad87
Lucky Setiawan <luckysetiawan0802@gmail.com>
2020-11-24 21:40:29 +0700

    Added Insert Item Feature

--- src/main/java/view/AddItemScreen.java
+++ src/main/java/view/AddItemScreen.java
@@ -6,6 +6,7 @@
 package view;
 
 import controller.Controller;
+import controller.UserManager;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -16,7 +17,8 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-//import javax.swing.SwingConstants;
+import javax.swing.SwingConstants;
+import model.Item;
 
 /**
  *
@@ -24,13 +26,12 @@ import javax.swing.JTextField;
  */
 public class AddItemScreen implements ActionListener {
     private JFrame menu;
-    private JPanel panelId, panelName, panelQuantity, panelSize, panelWeight, panelPrice;
-    private JLabel labelTitle ,labelItemId ,labelItemName,labelItemQuantity,labelItemSize;
-    private JLabel labelItemWeight, labelItemPrice, labelItemIsDeleted;
-    private JTextField textItemId, textItemName, textItemQuantity, textItemSize;
-    private JTextField textItemWeight, textItemPrice;
-    private JButton btnBack,btnSubmit;
-    Controller control = new Controller();
+    private JPanel panelName, panelQuantity, panelSize, panelWeight, panelPrice, panelButton;
+    private JLabel labelTitle ,labelName,labelQuantity,labelSize;
+    private JLabel labelWeight, labelPrice;
+    private JTextField textName, textQuantity, textSize, textWeight, textPrice;
+    private JButton btnBack,btnUpdate;
+    private Item selectedItem;
     
     public AddItemScreen() {
         showAddItem();
@@ -40,85 +41,58 @@ public class AddItemScreen implements ActionListener {
         menu = new JFrame("Add Item");
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         menu.setSize(512, 256);
-        menu.setLayout(new GridLayout(2, 1));
+        menu.setLayout(new GridLayout(7, 1));
         
-        labelTitle = new JLabel("Add Item" /*, SwingConstants.CENTER*/);
-        labelTitle.setBounds(200,0,100,100);
+        labelTitle = new JLabel("Add Item", SwingConstants.CENTER);
         labelTitle.setFont(new Font("Georia", Font.BOLD, 20));
-
-        labelItemId = new JLabel();
-        labelItemId.setText("Item Id : ");
-        labelItemId.setBounds(10, 30, 100, 100);
-        menu.add(labelItemId);
-        textItemId = new JTextField();
-        textItemId.setBounds(130, 70, 300, 20);
-        menu.add(textItemId);
-            
-        //item name
-        labelItemName = new JLabel();
-        labelItemName.setText("Item Name : ");
-        labelItemName.setBounds(10, 50, 100, 100);
-        menu.add(labelItemName);
-        textItemName = new JTextField();
-        textItemName.setBounds(130, 90, 300, 20);
-        menu.add(textItemName);
-            
-        //item quantity
-        labelItemQuantity = new JLabel();
-        labelItemQuantity.setText("Item Quantity : ");
-        labelItemQuantity.setBounds(10, 70, 100, 100);
-        menu.add(labelItemQuantity);
-        textItemQuantity = new JTextField();
-        textItemQuantity.setBounds(130, 110, 300, 20);
-        menu.add(textItemQuantity);
         
-        //item size
-        labelItemSize = new JLabel();
-        labelItemSize.setText("Item Size : ");
-        labelItemSize.setBounds(10, 90, 100, 100);
-        menu.add(labelItemSize);
-        textItemSize = new JTextField();
-        textItemSize.setBounds(130, 130, 300, 20);
-        menu.add(textItemSize);
+        panelName = new JPanel(new GridLayout(1, 2));
+        labelName = new JLabel("Name : ");
+        textName = new JTextField(selectedItem.getItem_name());
+        panelName.add(labelName);
+        panelName.add(textName);
         
-        //item weight
-        labelItemWeight = new JLabel();
-        labelItemWeight.setText("Item Weight : ");
-        labelItemWeight.setBounds(10, 110, 100, 100);
-        menu.add(labelItemWeight);
-        textItemWeight = new JTextField();
-        textItemWeight.setBounds(130, 150, 300, 20);
-        menu.add(textItemWeight);
+        panelQuantity = new JPanel(new GridLayout(1, 2));
+        labelQuantity = new JLabel("Quantity : ");
+        textQuantity = new JTextField("" + selectedItem.getItem_quantity());
+        panelQuantity.add(labelQuantity);
+        panelQuantity.add(textQuantity);
         
-        //item price
-        labelItemPrice = new JLabel();
-        labelItemPrice.setText("Item Price : ");
-        labelItemPrice.setBounds(10, 130, 100, 100);
-        menu.add(labelItemPrice);
-        textItemPrice = new JTextField();
-        textItemPrice.setBounds(130, 170, 300, 20);
-        menu.add(textItemPrice);
-
-        btnSubmit = new JButton("Submit");
-        btnSubmit.setActionCommand("submit");
-        btnSubmit.addActionListener(this);
-        btnSubmit.setBounds(20,220,100,20);
-        menu.add(btnSubmit);
+        panelSize = new JPanel(new GridLayout(1, 2));
+        labelSize = new JLabel("Size : ");
+        textSize = new JTextField("" + selectedItem.getItem_size());
+        panelSize.add(labelSize);
+        panelSize.add(textSize);
         
+        panelWeight = new JPanel(new GridLayout(1, 2));
+        labelWeight = new JLabel("Weight : ");
+        textWeight = new JTextField("" + selectedItem.getItem_weight());
+        panelWeight.add(labelWeight);
+        panelWeight.add(textWeight);
+        
+        panelPrice = new JPanel(new GridLayout(1, 2));
+        labelPrice = new JLabel("Price : ");
+        textPrice = new JTextField("" + selectedItem.getItem_price());
+        panelPrice.add(labelPrice);
+        panelPrice.add(textPrice);
+        
+        panelButton = new JPanel(new GridLayout(1, 2));
         btnBack = new JButton("Back");
         btnBack.setActionCommand("back");
-        btnBack.setBounds(130,220,100,20);
         btnBack.addActionListener(this);
+        btnUpdate = new JButton("Submit");
+        btnUpdate.setActionCommand("submit");
+        btnUpdate.addActionListener(this);
+        panelButton.add(btnBack);
+        panelButton.add(btnUpdate);
         
-//        //*red alert
-//        JLabel labelItemPrice = new JLabel();
-//        labelItemPrice.setText("Data Item Tidak Lengkap");
-//        labelItemPrice.setBounds(50, 190, 100, 100);
-//        menu.add(labelItemPrice);
-//        labelItemPrice.setVisible(false);
-                
         menu.add(labelTitle);
-        menu.add(btnBack);
+        menu.add(panelName);
+        menu.add(panelQuantity);
+        menu.add(panelSize);
+        menu.add(panelWeight);
+        menu.add(panelPrice);
+        menu.add(panelButton);
         menu.setVisible(true);
     }
 
@@ -131,22 +105,22 @@ public class AddItemScreen implements ActionListener {
                 new MainMenuScreen();
                 break;
             case "submit":
-                if(textItemId.equals("")||textItemName.equals("")||textItemQuantity.equals("")||
-                    textItemSize.equals("")||textItemWeight.equals("")||
-                    textItemPrice.equals("")){
-//                   strItemIdlabelItemPrice.setVisible(true);
+                String name = textName.getText();
+                String quantity = textQuantity.getText();
+                String size = textSize.getText();
+                String weight = textWeight.getText();
+                String price = textPrice.getText();
+                
+                if(name.equals("") || quantity.equals("") || size.equals("") || 
+                        weight.equals("") || price.equals("")){
                     JOptionPane.showMessageDialog(null,"Data Item Tidak Lengkap!","Alert",JOptionPane.WARNING_MESSAGE);
                 }else{
-                    String strItemId = textItemId.getText();
-                    String strItemName = textItemName.getText();
-                    String intItemQuantity = textItemQuantity.getText();
-                    String intItemSize = textItemSize.getText();
-                    String intItemWeight = textItemWeight.getText();
-                    String intItemPrice = textItemPrice.getText();
-                   
-//                    Controller.insertItem(strItemId, strItemName, intItemQuantity, intItemSize, intItemWeight, intItemPrice);
+                    int quantityNum = Integer.parseInt(textQuantity.getText());
+                    int sizeNum = Integer.parseInt(textSize.getText());
+                    int weightNum = Integer.parseInt(textWeight.getText());
+                    int priceNum = Integer.parseInt(textPrice.getText());
                     
-                    
+                    Controller.insertItem(UserManager.getInstance().getUser().getUid(), name, quantityNum, sizeNum, weightNum, priceNum);
                 }
             default:
                 throw new IllegalStateException("Unexpected value: " + command);
